6368d8051c70976fa0befee3519f35e1174502d4
Dark-97o <darksoul97.anderson@gmail.com>
2026-05-09 16:45:41 +0530

    style: fix alignment of dashboard stat cards on tablet

--- src/pages/NFTShop.jsx
+++ src/pages/NFTShop.jsx
@@ -454,3 +454,5 @@ export default function NFTShop({ role }) {
 // docs: update README with local setup instructions [v7.7.2-2026-05-04]
 
 // chore: update vite to latest patch version [v8.0.89-2026-05-09]
+
+// style: fix alignment of dashboard stat cards on tablet [v5.4.42-2026-05-09]
