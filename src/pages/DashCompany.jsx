e1366440116d759b02ae251a4bb2cfed2ec42bd3
Dark-97o <darksoul97.anderson@gmail.com>
2026-05-09 16:42:25 +0530

    chore: bump stellar-sdk to v15.0.1

--- src/pages/DashCompany.jsx
+++ src/pages/DashCompany.jsx
@@ -1216,3 +1216,5 @@ export default DashCompany;
 // docs: add code comments to Firebase initialization [v8.4.30-2026-05-02]
 
 // style: improve contrast ratio on secondary text elements [v8.7.8-2026-05-09]
+
+// chore: bump stellar-sdk to v15.0.1 [v6.7.64-2026-05-09]
