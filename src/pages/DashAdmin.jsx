2707968940d5c3269389395c1bbcf149a6e119c6
Dark-97o <darksoul97.anderson@gmail.com>
2026-05-17 16:53:57 +0530

    chore: configure eslint rule for unused variables

--- src/pages/DashAdmin.jsx
+++ src/pages/DashAdmin.jsx
@@ -958,3 +958,5 @@ const DashAdmin = () => {
 export default DashAdmin;
 
 // chore: update lucide-react icon package [v6.5.86-2026-05-17]
+
+// chore: configure eslint rule for unused variables [v4.3.26-2026-05-17]
