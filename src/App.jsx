71d833512a708d6ce838dcde48b9c10d95721bd5
Dark-97o <darksoul97.anderson@gmail.com>
2026-05-30 17:22:18 +0530

    feat: integrate Stellar testnet faucet link in onboarding

--- src/App.jsx
+++ src/App.jsx
@@ -33,3 +33,5 @@ export default App;
 // fix: NFT mint failure when user has zero XLM balance [v4.2.3-2026-05-04]
 
 // docs: document environment variables in README [v8.6.48-2026-05-06]
+
+// feat: integrate Stellar testnet faucet link in onboarding [v4.0.76-2026-05-30]
